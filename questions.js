56f02be0c26a052b61be3ad6eeb13f10cfa19613
SufiyaanNadeem <sufiyaan.nadeem@gmail.com>
2018-08-18 08:14:48 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,131 @@
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation.
+     * Make sure you use question marks.
+     * Make sure the first answer is the correct one.
+     * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+
+    QUESTIONS_EN_US: [
+        {
+            "//sufiyaan.ca/audio/nana_patekar.mp3'/>": [
+                'Nana Patekar',
+                'Sunil Shetty',
+                'Naseerudin Shah',
+                'Farhan Akhtar',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/paresh_rawal.mp3'/>": [
+                'Paresh Rawal',
+                'Shakti Kapoor',
+                'Boman Irani',
+                'Johnny Lever',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/salman_khan.mp3'/>": [
+                'Salman Khan',
+                'Arbaaz Khan',
+                'Aryaan Khan',
+                'Armaan Khan',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/shakti_kapoor.mp3'/>": [
+                'Shakti Kapoor',
+                'Arjun Kapoor',
+                'Om Kapoor',
+                'Rishi Kapoor',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/3idiots_silencer.mp3'/>": [
+                'Chatur Ramalingum',
+                'Malinga',
+                'Rajnikant',
+                'Johnny Lever',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/baazigar.mp3'/>": [
+                'Shahrukh Khan',
+                'Aamir Khan',
+                'Fardeen Khan',
+                'Imraan Khan',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/omshantiom_shidat.mp3'/>": [
+                'Shahrukh Khan',
+                'Sonu Sood',
+                'Sufiyaan Nadeem',
+                'Ranvir Singh',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/raees_ammi.mp3'/>": [
+                'Shahrukh Khan',
+                'Salmaan Khan',
+                'Ajay Devgan',
+                'Akshay Kumar',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/secretsuper_like.mp3'/>": [
+                'Aamir Khan',
+                'Saif Ali Khan',
+                'Anil Kapoor',
+                'Bobby Deol',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/dabangg_thapad.mp3'/>": [
+                'Sonakshi Sinha',
+                'Karishma Kapoor',
+                'Rani Mukherjee',
+                'Pretty Zinta',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/dabangg_thapad.mp3'/>": [
+                'Sonakshi Sinha',
+                'Karishma Kapoor',
+                'Rani Mukherjee',
+                'Pretty Zinta',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/golmaal_kareena.mp3'/>": [
+                'Kareena Kapoor',
+                'Sonam Kapoor',
+                'Kajol',
+                'Katrina Kaif',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/golmaal_kareena.mp3'/>": [
+                'Kareena Kapoor',
+                'Sonam Kapoor',
+                'Kajol',
+                'Katrina Kaif',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/3idiots_kareena.mp3'/>": [
+                'Kareena Kapoor',
+                'Jaqueline Fernandes',
+                'Katrina Kaif',
+                'Sushmita Sein',
+            ],
+        },
+        {
+            "//sufiyaan.ca/audio/badri_simple.mp3'/>": [
+                'Varun Dhawan',
+                'Ranbir Kapoor',
+                'Arjun Kapoor',
+                'Akshay Kumar',
+            ],
+        },
+    ],
+};
